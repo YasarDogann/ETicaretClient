39238dea12956139e5d374f3731e58ba63ebcf28
YasarDogann <yasardgn99@gmail.com>
2025-06-26 13:25:02 +0300

    Ürün Listeleme ve Pagination

--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -1,24 +1,18 @@
-import { Injectable } from '@angular/core';
-import { HttpClientService } from '../http-client.service';
-import { Create_Product } from '../../../contracts/create_product';
 import { HttpErrorResponse } from '@angular/common/http';
-import { List_Product } from '../../../contracts/list_product';
+import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
+import { Create_Product } from '../../../contracts/create_product';
+import { List_Product } from '../../../contracts/list_product';
 import { List_Product_Image } from '../../../contracts/list_product_image';
+import { HttpClientService } from '../http-client.service';
 
 @Injectable({
   providedIn: 'root'
 })
-
-// VERİ EKLEME VE ÇEKME İŞLEMLERİNİ BU SERVİS ÜZERİNDEN YÖNETTİM
 export class ProductService {
-  changeShowcaseImage(imageId: string, arg1: string, arg2: () => void) {
-    throw new Error('Method not implemented.');
-  }
-
   constructor(private httpClientService: HttpClientService) { }
 
-  create(product: Create_Product, successCallBack ?: any, errorCallBack? : (errorMessage: string) => void){
+  create(product: Create_Product, successCallBack?: () => void, errorCallBack?: (errorMessage: string) => void) {
     this.httpClientService.post({
       controller: "products"
     }, product)
@@ -36,8 +30,8 @@ export class ProductService {
       });
   }
 
-  async read(page: number = 0, size: number = 5, successCallBack?: () => void, errorCallBack?: (errorMessage: string) => void): Promise<{totalCount: number; products: List_Product[]}> {
-    const promiseData : Promise<{totalCount: number; products: List_Product[]}> = this.httpClientService.get<{ totalCount: number; products: List_Product[] }> ({
+  async read(page: number = 0, size: number = 5, successCallBack?: () => void, errorCallBack?: (errorMessage: string) => void): Promise<{ totalProductCount: number; products: List_Product[] }> {
+    const promiseData: Promise<{ totalProductCount: number; products: List_Product[] }> = this.httpClientService.get<{ totalProductCount: number; products: List_Product[] }>({
       controller: "products",
       queryString: `page=${page}&size=${size}`
     }).toPromise();
@@ -45,10 +39,10 @@ export class ProductService {
     promiseData.then(d => successCallBack())
       .catch((errorResponse: HttpErrorResponse) => errorCallBack(errorResponse.message))
 
-      return await promiseData;
+    return await promiseData;
   }
 
-  async delete(id: string){
+  async delete(id: string) {
     const deleteObservable: Observable<any> = this.httpClientService.delete<any>({
       controller: "products"
     }, id);
@@ -56,18 +50,17 @@ export class ProductService {
     await firstValueFrom(deleteObservable);
   }
 
-  async readImages(id: string, successCallBack?: () => void): Promise<List_Product_Image[]>{
+  async readImages(id: string, successCallBack?: () => void): Promise<List_Product_Image[]> {
     const getObservable: Observable<List_Product_Image[]> = this.httpClientService.get<List_Product_Image[]>({
       action: "getproductimages",
       controller: "products"
     }, id);
 
-    const images : List_Product_Image[] = await firstValueFrom(getObservable);
+    const images: List_Product_Image[] = await firstValueFrom(getObservable);
     successCallBack();
     return images;
   }
 
-  // resim silme
   async deleteImage(id: string, imageId: string, successCallBack?: () => void) {
     const deleteObservable = this.httpClientService.delete({
       action: "deleteproductimage",
@@ -75,6 +68,28 @@ export class ProductService {
       queryString: `imageId=${imageId}`
     }, id)
     await firstValueFrom(deleteObservable);
+    successCallBack();
+  }
+
+  async changeShowcaseImage(imageId: string, productId: string, successCallBack?: () => void): Promise<void> {
+    const changeShowcaseImageObservable = this.httpClientService.get({
+      controller: "products",
+      action: "ChangeShowcaseImage",
+      queryString: `imageId=${imageId}&productId=${productId}`
+    });
+    await firstValueFrom(changeShowcaseImageObservable);
+    successCallBack();
+  }
+
+  async updateStockQrCodeToProduct(productId: string, stock: number, successCallBack?: () => void) {
+    const observable = this.httpClientService.put({
+      action: "qrcode",
+      controller: "products"
+    }, {
+      productId, stock
+    });
+
+    await firstValueFrom(observable);
     successCallBack();
   }
 }
